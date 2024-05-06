1aeb3ff9bafe175f504e2cb165f32df28f843a1b
jdadsm <joaomourao2002@gmail.com>
2024-05-06 15:35:46 +0100

    Added menu to interact with program

--- src/main.cpp
+++ src/main.cpp
@@ -8,17 +8,32 @@
 using namespace std;
 
 
-int main() {
+int main(int argc, char* argv[]) {
     int k = 2;
     int alpha = 1;
-    string nameToSaveModel = "model1.bin";
-
-    MainClass main(k,alpha);
-
+    string nameToSaveModel = "";
     string filePathPos = "data/pos_data_train.csv";
     string filePathNeg = "data/neg_data_train.csv";
     string filePathPosTest = "data/pos_data_test.csv";
     string filePathNegTest = "data/neg_data_test.csv";
+
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+        if (arg == "-fp" && i + 1 < argc) {
+            filePathPos = argv[++i];
+        } else if (arg == "-fn" && i + 1 < argc) {
+            filePathNeg = argv[++i];
+        } else if (arg == "-k" && i + 1 < argc) {
+            k = std::stoi(argv[++i]);
+        } else if (arg == "-a" && i + 1 < argc) {
+            alpha = std::stoi(argv[++i]);
+        } else if (arg == "-s" && i + 1 < argc) {
+            nameToSaveModel = argv[++i];
+        }
+    }
+
+    MainClass main(k,alpha);
+
     main.readData(filePathPos,filePathNeg,filePathPosTest,filePathNegTest);
 
     main.trainModels();
